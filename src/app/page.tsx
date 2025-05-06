c6002089d46a9a4eb922422546b66bb8af57dca0
2YH02 <yonghuni484@gmail.com>
2025-05-06 21:55:20 +0900

    Feat: 메인 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,68 @@
-import Image from "next/image";
+import { MaskContainer } from "@/components/ui/MaskContainer";
+import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative w-screen h-screen bg-black overflow-hidden">
+      <button>asd</button>
+      <Spotlight
+        className="-top-40 left-0 md:-top-20 md:left-60"
+        fill="#a8a8a8"
+      />
+      <MaskContainer
+        revealText={
+          <div className="flex flex-col">
+            <p className="mx-auto max-w-4xl text-center font-bold text-slate-500 dark:text-white text-sm md:text-2xl lg:text-4xl">
+              최적화된 코드로, 인터랙티브한 경험을 만들고,
+              <br />
+              사용자 중심의 웹을 고민합니다.
+            </p>
+            <div className="flex gap-8 justify-center text-white text-base mt-3">
+              <span>projects</span>
+              <span>about me</span>
+              <span>post</span>
+            </div>
+          </div>
+        }
+        className="h-full w-full text-white dark:text-black hidden md:block"
+      >
+        <div className="font-nanum text-2xl md:text-4xl lg:text-[44px]">
+          <span className="text-blue-500">퍼포먼스 최적화</span>와{" "}
+          <span className="text-blue-500">인터랙션</span>을 사랑하는
+          <br />
+          <span className="text-blue-500">프론트엔드 개발자</span> 이용훈입니다.
+          <div className="flex gap-8 justify-center text-white mt-3 text-lg md:text-xl lg:text-2xl">
+            <a href="" className="hover:animate-shake-rotate">
+              projects
+            </a>
+            <a href="" className="hover:animate-shake-rotate">
+              about me
+            </a>
+            <a href="" className="hover:animate-shake-rotate">
+              post
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </MaskContainer>
+      <div className="absolute top-1/2 left-1/2 w-full -translate-1/2 text-center text-white dark:text-black">
+        <div className="font-nanum text-2xl md:text-4xl lg:text-[44px] md:hidden lg:hidden">
+          <span className="text-blue-500">퍼포먼스 최적화</span>와{" "}
+          <span className="text-blue-500">인터랙션</span>을 사랑하는
+          <br />
+          <span className="text-blue-500">프론트엔드 개발자</span> 이용훈입니다.
+          <div className="flex gap-8 justify-center text-white mt-3 text-lg md:text-xl lg:text-2xl">
+            <a href="" className="active:animate-shake-rotate">
+              projects
+            </a>
+            <a href="" className="active:animate-shake-rotate">
+              about me
+            </a>
+            <a href="" className="active:animate-shake-rotate">
+              post
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
