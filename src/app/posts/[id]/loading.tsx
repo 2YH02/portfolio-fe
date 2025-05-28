3bed9b805a70f44528d5fff4d401038284edd32e
2YH02 <yonghuni484@gmail.com>
2025-05-28 18:51:35 +0900

    Style: 포스팅 상세 로딩 UI 스타일 수정

--- src/app/posts/[id]/loading.tsx
+++ src/app/posts/[id]/loading.tsx
@@ -15,15 +15,16 @@ export default function Loading() {
         </div>
         {/* 본문 텍스트 블록 */}
         <div className="space-y-2 mt-8">
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-4/6" />
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-3/5" />
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-2/3" />
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-4/5" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-4/6" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-3/5" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-2/3" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-4/5" />
           <div className="mb-6" />
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-4/6" />
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-3/5" />
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-2/3" />
-          <div className="h-4 bg-gray-300 animate-pulse rounded w-4/5" />
+          <div className="h-10 mb-4 bg-gray-300 animate-pulse rounded w-2/12" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-4/6" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-3/5" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-2/3" />
+          <div className="h-5 bg-gray-300 animate-pulse rounded w-4/5" />
         </div>
       </div>
     </div>
