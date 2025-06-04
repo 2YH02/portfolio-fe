ebb5146b9a09d8c234d126b11b36eeb5a2ba69fb
2YH02 <yonghuni484@gmail.com>
2025-06-04 17:39:36 +0900

    Feat: 구글 애널리틱스 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import GoogleAnalytics from "@/components/provider/GoogleAnalytics";
 import InitHLJS from "@/components/provider/InitHLJS";
 import PathListener from "@/components/provider/PathListener";
 import NoiseBackground from "@/components/ui/NoiseBackground";
@@ -22,8 +23,22 @@ const geistNanum = Nanum_Pen_Script({
 });
 
 export const metadata: Metadata = {
-  title: "Yonghun",
-  description: "포트폴리오, 블로그, 그리고 나",
+  title: "Yonghun - 포트폴리오",
+  description: "기억에 남는 순간을 만들고 싶은 웹 개발자 이용훈입니다.",
+  keywords: "웹개발,프론트엔드,백엔드,포트폴리오,개발자,블로그",
+  openGraph: {
+    type: "website",
+    url: "https://www.yonghun.me",
+    title: "Yonghun - 포트폴리오",
+    description: "기억에 남는 순간을 만들고 싶은 웹 개발자 이용훈입니다.",
+    images: "/metaimg.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Yonghun - 포트폴리오",
+    description: "기억에 남는 순간을 만들고 싶은 웹 개발자 이용훈입니다.",
+    images: "/metaimg.png",
+  },
 };
 
 export default function RootLayout({
@@ -36,6 +51,9 @@ export default function RootLayout({
       <body
         className={`${geistMono.variable} ${geistSans.variable} ${geistNanum} antialiased overflow-hidden`}
       >
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <InitHLJS />
         <PathListener />
         <NoiseBackground />
